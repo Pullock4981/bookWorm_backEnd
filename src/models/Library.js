10ba77617db9101ad304d67c7e5829fa73cfe16b
Syed Ashik Mahmud Pullock <pullock15-4981@diu.edu.bd>
2026-01-12 22:09:09 +0600

    add library model

---
+++ src/models/Library.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+/**
+ * Library Model Schema
+ * Tracks user's personal book collection, reading shelves, and progress.
+ */
+const librarySchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: [true, 'Library entry must belong to a user']
+    },
+    book: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Book',
+        required: [true, 'Library entry must belong to a book']
+    },
+    shelf: {
+        type: String,
+        enum: ['Want to Read', 'Currently Reading', 'Read'],
+        required: [true, 'Shelf name is required']
+    },
+    pagesRead: {
+        type: Number,
+        default: 0,
+        min: [0, 'Pages read cannot be negative']
+    },
+    totalPages: {
+        type: Number,
+        default: 0
+    }
+}, {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+});
+
+// Ensure a user can only have one library record per book
+librarySchema.index({ user: 1, book: 1 }, { unique: true });
+
+/**
+ * Middleware: Automatically populate book info when fetching library records
+ */
+librarySchema.pre(/^find/, function (next) {
+    this.populate({
+        path: 'book',
+        select: 'title author coverImage genre totalPages averageRating'
+    });
+    next();
+});
+
+const Library = mongoose.model('Library', librarySchema);
+
+module.exports = Library;
