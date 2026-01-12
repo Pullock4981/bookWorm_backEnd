9f78f3f4ba2f1c074729f846a67785ce3b440ec0
Syed Ashik Mahmud Pullock <pullock15-4981@diu.edu.bd>
2026-01-12 14:34:53 +0600

    add book model

---
+++ src/models/Book.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+
+/**
+ * Book Model Schema
+ * Represents a book in the system with its metadata and association to a Genre.
+ */
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'A book must have a title'],
+        trim: true
+    },
+    author: {
+        type: String,
+        required: [true, 'A book must have an author'],
+        trim: true
+    },
+    genre: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Genre',
+        required: [true, 'A book must belong to a genre']
+    },
+    description: {
+        type: String,
+        required: [true, 'A book must have a description']
+    },
+    coverImage: {
+        type: String,
+        required: [true, 'A book must have a cover image']
+    },
+    pdfUrl: {
+        type: String,
+        required: [true, 'A book must have a PDF file']
+    },
+    totalPages: {
+        type: Number,
+        default: 0
+    },
+    averageRating: {
+        type: Number,
+        default: 0,
+        min: [0, 'Rating must be above 0'],
+        max: [5, 'Rating must be below 5']
+    },
+    totalReviews: {
+        type: Number,
+        default: 0
+    }
+}, {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+});
+
+/**
+ * Query Middleware: Automatically populate the genre name when fetching books
+ */
+bookSchema.pre(/^find/, function (next) {
+    this.populate({
+        path: 'genre',
+        select: 'name'
+    });
+    next();
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
